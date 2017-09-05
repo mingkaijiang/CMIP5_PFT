12e72c7a17c076b51d5f2458327bd2441c35c9a1
mingkaijiang <shanyujordan512@gmail.com>
2017-09-05 20:03:59 +1000

    pasted relevant scripts

---
+++ R/prepare_R.R
@@ -0,0 +1,25 @@
+
+#### Prepare folder structure and libraries
+####
+#### 
+################################################################################
+
+#### Install libraries
+if(!require(pacman))install.packages("pacman")
+pacman::p_load(scatterplot3d, 
+               data.table, 
+               lattice, 
+               plyr,
+               rPython,
+               grid,
+               gridBase,
+               ini,
+               ncdf4) # add other packages needed to this list
+
+
+#### Sourcing all R files in the function subdirectory
+sourcefiles <- dir("Functions", pattern="[.]R$", recursive = TRUE, full.names = TRUE)
+for(z in sourcefiles)source(z)
+
+
+op <- par()
---
+++ Run_programs.R
@@ -0,0 +1,27 @@
+
+#### Main program
+###
+### To process CMIP5 vegetation data for australia
+###
+### Author: Mingkai Jiang (m.jiang@westernsydney.edu.au)
+### 
+#### ------------------------ General system stuffs ------------------------ #####
+### Make sure everything is clear
+rm(list=ls(all=TRUE))
+
+### Get current date
+date<-Sys.Date()
+
+### read in all R packages
+source("R/prepare_R.R")
+
+#### ------------------------ Process raw data nc files ------------------------ #####
+
+
+
+##### ------------------------ Clear workspace ---------------------------- #####
+rm(list=ls(all=TRUE))
+
+#### End
+
+
