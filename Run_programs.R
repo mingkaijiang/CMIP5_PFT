310374cc19d4df00b8ff42d2cd680ca357fabcd4
mingkaijiang <shanyujordan512@gmail.com>
2017-09-14 09:10:25 +1000

    finished programming, need to test next

--- Run_programs.R
+++ Run_programs.R
@@ -14,6 +14,9 @@ source("R/prepare_R.R")
 
 #### ------------------------ Process raw nc files ------------------------ #####
 
+system("mkdir Data/Processed")
+
+
 # RCP 26
 nc_to_csv(Datfile = "landCoverFrac_Lmon_MIROC-ESM_rcp26_r1i1p1_200601-210012", 
           sourceDir = "Data/Raw", destDir = "Data/Processed/MIROC-ESM_rcp26")
@@ -112,10 +115,12 @@ nc_to_csv(Datfile = "landCoverFrac_Lmon_MRI-ESM1_rcp85_r1i1p1_200601-210012",
 
 ##### ------------------------ Plotting ---------------------------- #####
 ### plot discret years to test
+system("mkdir Plots/Discret")
 Aus_Vegetation_Plot_Discret(sourceDir = "Data/Processed/MIROC-ESM_rcp26", 
                             destDir = "Plots/Discret/MIROC-ESM_rcp26")
 
 ### plot continuous year plots within the same pdf file
+system("mkdir Plots/Continuous")
 ## RCP 26
 Aus_Vegetation_Plot_Continuous(sourceDir = "Data/Processed/MIROC-ESM_rcp26",
                                destDir = "Plots/Continuous/MIROC-ESM_rcp26")
@@ -208,6 +213,103 @@ Aus_Vegetation_Plot_Continuous(sourceDir = "Data/Processed/MPI-ESM-MR_rcp85",
 
 Aus_Vegetation_Plot_Continuous(sourceDir = "Data/Processed/MRI-ESM1_rcp85",
                                destDir = "Plots/Continuous/MRI-ESM1_rcp85")
+
+
+##### ------------------------ Animated plots ---------------------------- #####
+system("mkdir Plots/Animated")
+
+## RCP 26
+animated_plot_command(sourceDir = "Plots/Continuous/MIROC-ESM_rcp26",
+                               destDir = "Plots/Animated/MIROC-ESM_rcp26")
+
+animated_plot_command(sourceDir = "Plots/Continuous/IPSL-CM5A-LR_rcp26",
+                               destDir = "Plots/Animated/IPSL-CM5A-LR_rcp26")
+
+animated_plot_command(sourceDir = "Plots/Continuous/IPSL-CM5A-MR_rcp26",
+                               destDir = "Plots/Animated/IPSL-CM5A-MR_rcp26")
+
+animated_plot_command(sourceDir = "Plots/Continuous/MIROC-ESM-CHEM_rcp26",
+                               destDir = "Plots/Animated/MIROC-ESM-CHEM_rcp26")
+
+animated_plot_command(sourceDir = "Plots/Continuous/MPI-ESM-LR_rcp26",
+                               destDir = "Plots/Animated/MPI-ESM-LR_rcp26")
+
+animated_plot_command(sourceDir = "Plots/Continuous/MPI-ESM-MR_rcp26",
+                               destDir = "Plots/Animated/MPI-ESM-MR_rcp26")
+
+## RCP 45
+animated_plot_command(sourceDir = "Plots/Continuous/ACCESS1-0_rcp45",
+                               destDir = "Plots/Animated/ACCESS1-0_rcp45")
+
+animated_plot_command(sourceDir = "Plots/Continuous/ACCESS1-3_rcp45",
+                               destDir = "Plots/Animated/ACCESS1-3_rcp45")
+
+animated_plot_command(sourceDir = "Plots/Continuous/IPSL-CM5A-LR_rcp45",
+                               destDir = "Plots/Animated/IPSL-CM5A-LR_rcp45")
+
+animated_plot_command(sourceDir = "Plots/Continuous/IPSL-CM5A-MR_rcp45",
+                               destDir = "Plots/Animated/IPSL-CM5A-MR_rcp45")
+
+animated_plot_command(sourceDir = "Plots/Continuous/IPSL-CM5B-LR_rcp45",
+                               destDir = "Plots/Animated/IPSL-CM5B-LR_rcp45")
+
+animated_plot_command(sourceDir = "Plots/Continuous/MIROC-ESM_rcp45",
+                               destDir = "Plots/Animated/MIROC-ESM_rcp45")
+
+animated_plot_command(sourceDir = "Plots/Continuous/MIROC-ESM-CHEM_rcp45",
+                               destDir = "Plots/Animated/MIROC-ESM-CHEM_rcp45")
+
+animated_plot_command(sourceDir = "Plots/Continuous/MPI-ESM-LR_rcp45",
+                               destDir = "Plots/Animated/MPI-ESM-LR_rcp45")
+
+animated_plot_command(sourceDir = "Plots/Continuous/MPI-ESM-MR_rcp45",
+                               destDir = "Plots/Animated/MPI-ESM-MR_rcp45")
+
+
+## RCP 60
+animated_plot_command(sourceDir = "Plots/Continuous/IPSL-CM5A-LR_rcp60",
+                               destDir = "Plots/Animated/IPSL-CM5A-LR_rcp60")
+
+animated_plot_command(sourceDir = "Plots/Continuous/IPSL-CM5A-MR_rcp60",
+                               destDir = "Plots/Animated/IPSL-CM5A-MR_rcp60")
+
+animated_plot_command(sourceDir = "Plots/Continuous/MIROC-ESM_rcp60",
+                               destDir = "Plots/Animated/MIROC-ESM_rcp60")
+
+animated_plot_command(sourceDir = "Plots/Continuous/MIROC-ESM-CHEM_rcp60",
+                               destDir = "Plots/Animated/MIROC-ESM-CHEM_rcp60")
+
+
+## RCP 85
+animated_plot_command(sourceDir = "Plots/Continuous/ACCESS1-0_rcp85",
+                               destDir = "Plots/Animated/ACCESS1-0_rcp85")
+
+animated_plot_command(sourceDir = "Plots/Continuous/ACCESS1-3_rcp85",
+                               destDir = "Plots/Animated/ACCESS1-3_rcp85")
+
+animated_plot_command(sourceDir = "Plots/Continuous/IPSL-CM5A-LR_rcp85",
+                               destDir = "Plots/Animated/IPSL-CM5A-LR_rcp85")
+
+animated_plot_command(sourceDir = "Plots/Continuous/IPSL-CM5A-MR_rcp85",
+                               destDir = "Plots/Animated/IPSL-CM5A-MR_rcp85")
+
+animated_plot_command(sourceDir = "Plots/Continuous/IPSL-CM5B-LR_rcp85",
+                               destDir = "Plots/Animated/IPSL-CM5B-LR_rcp85")
+
+animated_plot_command(sourceDir = "Plots/Continuous/MIROC-ESM_rcp85",
+                               destDir = "Plots/Animated/MIROC-ESM_rcp85")
+
+animated_plot_command(sourceDir = "Plots/Continuous/MIROC-ESM-CHEM_rcp85",
+                               destDir = "Plots/Animated/MIROC-ESM-CHEM_rcp85")
+
+animated_plot_command(sourceDir = "Plots/Continuous/MPI-ESM-LR_rcp85",
+                               destDir = "Plots/Animated/MPI-ESM-LR_rcp85")
+
+animated_plot_command(sourceDir = "Plots/Continuous/MPI-ESM-MR_rcp85",
+                               destDir = "Plots/Animated/MPI-ESM-MR_rcp85")
+
+animated_plot_command(sourceDir = "Plots/Continuous/MRI-ESM1_rcp85",
+                               destDir = "Plots/Animated/MRI-ESM1_rcp85")
 
 
 
--- modules/animated_plot_command.R
+++ modules/animated_plot_command.R
@@ -1,11 +1,26 @@
 animated_plot_command <- function(sourceDir, destDir) {
     
+    dir.create(destDir, showWarnings = FALSE)
+    DatFiles <- list.files(path = sourceDir, pattern = "\\.pdf")
     
-    system("convert -delay 80 *.png example_1.gif")
+    for (i in 1:length(DatFiles)) 
+    {
+        print(DatFiles[i])
+        
+        # prepare outfile name
+        oName <- gsub(".pdf", ".gif", DatFiles[i])
+        outName <- paste0(destDir, "/", oName)
+        
+        inName <- file.path(sourceDir, DatFiles[i], fsep = .Platform$file.sep)
+    
+        # prepare command line text
+        s.cmd <- paste0("convert -delay 80 ", inName, " ", outName)
+        
+        system(s.cmd)
+    }
     
     # to not leave the directory with the single jpeg files
     # I remove them.
-    file.remove(list.files(pattern=".png"))
-    
+    # file.remove(list.files(pattern=".pdf"))
     
 }
