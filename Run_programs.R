458d1279f48b92b220f57d2b78fcc31c38eb55cd
mingkaijiang <shanyujordan512@gmail.com>
2017-09-14 08:54:22 +1000

    added function to plot continous plots

--- Run_programs.R
+++ Run_programs.R
@@ -12,23 +12,17 @@ rm(list=ls(all=TRUE))
 ### read in all R packages
 source("R/prepare_R.R")
 
-Datfile = "landCoverFrac_Lmon_IPSL-CM5A-LR_rcp26_r1i1p1_200601-230012"
-sourceDir = "Data/Raw"
-destDir = "Data/Processed/IPSL-CM5A-LR_rcp26"
-
-
 #### ------------------------ Process raw nc files ------------------------ #####
 
 # RCP 26
-
 nc_to_csv(Datfile = "landCoverFrac_Lmon_MIROC-ESM_rcp26_r1i1p1_200601-210012", 
           sourceDir = "Data/Raw", destDir = "Data/Processed/MIROC-ESM_rcp26")
 
-nc_to_csv(Datfile = "landCoverFrac_Lmon_IPSL-CM5A-LR_rcp26_r1i1p1_200601-230012", 
-          sourceDir = "Data/Raw", destDir = "Data/Processed/IPSL-CM5A-LR_rcp26")
+nc_to_csv_IPSL(Datfile = "landCoverFrac_Lmon_IPSL-CM5A-LR_rcp26_r1i1p1_200601-230012", 
+               sourceDir = "Data/Raw", destDir = "Data/Processed/IPSL-CM5A-LR_rcp26")
 
-nc_to_csv(Datfile = "landCoverFrac_Lmon_IPSL-CM5A-MR_rcp26_r1i1p1_200601-210012", 
-          sourceDir = "Data/Raw", destDir = "Data/Processed/IPSL-CM5A-MR_rcp26")
+nc_to_csv_IPSL(Datfile = "landCoverFrac_Lmon_IPSL-CM5A-MR_rcp26_r1i1p1_200601-210012", 
+               sourceDir = "Data/Raw", destDir = "Data/Processed/IPSL-CM5A-MR_rcp26")
 
 nc_to_csv(Datfile = "landCoverFrac_Lmon_MIROC-ESM-CHEM_rcp26_r1i1p1_200601-210012", 
           sourceDir = "Data/Raw", destDir = "Data/Processed/MIROC-ESM-CHEM_rcp26")
@@ -42,19 +36,19 @@ nc_to_csv(Datfile = "landCoverFrac_Lmon_MPI-ESM-MR_rcp26_r1i1p1_200601-210012",
 
 # RCP 45
 nc_to_csv_ACCESS(Datfile = "landCoverFrac_Lmon_ACCESS1-0_rcp45_r1i1p1_200601-200601", 
-          sourceDir = "Data/Raw", destDir = "Data/Processed/ACCESS1-0_rcp45")
+                 sourceDir = "Data/Raw", destDir = "Data/Processed/ACCESS1-0_rcp45")
 
 nc_to_csv_ACCESS(Datfile = "landCoverFrac_Lmon_ACCESS1-3_rcp45_r1i1p1_200601-200601", 
-          sourceDir = "Data/Raw", destDir = "Data/Processed/ACCESS1-3_rcp45")
+                 sourceDir = "Data/Raw", destDir = "Data/Processed/ACCESS1-3_rcp45")
 
-nc_to_csv(Datfile = "landCoverFrac_Lmon_IPSL-CM5A-LR_rcp45_r1i1p1_200601-230012", 
-          sourceDir = "Data/Raw", destDir = "Data/Processed/IPSL-CM5A-LR_rcp45")
+nc_to_csv_IPSL(Datfile = "landCoverFrac_Lmon_IPSL-CM5A-LR_rcp45_r1i1p1_200601-230012", 
+               sourceDir = "Data/Raw", destDir = "Data/Processed/IPSL-CM5A-LR_rcp45")
 
-nc_to_csv(Datfile = "landCoverFrac_Lmon_IPSL-CM5A-MR_rcp45_r1i1p1_200601-210012", 
-          sourceDir = "Data/Raw", destDir = "Data/Processed/IPSL-CM5A-MR_rcp45")
+nc_to_csv_IPSL(Datfile = "landCoverFrac_Lmon_IPSL-CM5A-MR_rcp45_r1i1p1_200601-210012", 
+               sourceDir = "Data/Raw", destDir = "Data/Processed/IPSL-CM5A-MR_rcp45")
 
-nc_to_csv(Datfile = "landCoverFrac_Lmon_IPSL-CM5B-LR_rcp45_r1i1p1_200601-210012", 
-          sourceDir = "Data/Raw", destDir = "Data/Processed/IPSL-CM5B-LR_rcp45")
+nc_to_csv_IPSL(Datfile = "landCoverFrac_Lmon_IPSL-CM5B-LR_rcp45_r1i1p1_200601-210012", 
+               sourceDir = "Data/Raw", destDir = "Data/Processed/IPSL-CM5B-LR_rcp45")
 
 nc_to_csv(Datfile = "landCoverFrac_Lmon_MIROC-ESM_rcp45_r1i1p1_200601-210012", 
           sourceDir = "Data/Raw", destDir = "Data/Processed/MIROC-ESM_rcp45")
@@ -68,12 +62,13 @@ nc_to_csv(Datfile = "landCoverFrac_Lmon_MPI-ESM-LR_rcp45_r1i1p1_200601-210012",
 nc_to_csv(Datfile = "landCoverFrac_Lmon_MPI-ESM-MR_rcp45_r1i1p1_200601-210012", 
           sourceDir = "Data/Raw", destDir = "Data/Processed/MPI-ESM-MR_rcp45")
 
-# RCP 60
-nc_to_csv(Datfile = "landCoverFrac_Lmon_IPSL-CM5A-LR_rcp60_r1i1p1_200601-210012", 
-          sourceDir = "Data/Raw", destDir = "Data/Processed/IPSL-CM5A-LR_rcp60")
 
-nc_to_csv(Datfile = "landCoverFrac_Lmon_IPSL-CM5A-MR_rcp60_r1i1p1_200601-210012", 
-          sourceDir = "Data/Raw", destDir = "Data/Processed/IPSL-CM5A-MR_rcp60")
+# RCP 60
+nc_to_csv_IPSL(Datfile = "landCoverFrac_Lmon_IPSL-CM5A-LR_rcp60_r1i1p1_200601-210012", 
+               sourceDir = "Data/Raw", destDir = "Data/Processed/IPSL-CM5A-LR_rcp60")
+
+nc_to_csv_IPSL(Datfile = "landCoverFrac_Lmon_IPSL-CM5A-MR_rcp60_r1i1p1_200601-210012", 
+               sourceDir = "Data/Raw", destDir = "Data/Processed/IPSL-CM5A-MR_rcp60")
 
 nc_to_csv(Datfile = "landCoverFrac_Lmon_MIROC-ESM_rcp60_r1i1p1_200601-210012", 
           sourceDir = "Data/Raw", destDir = "Data/Processed/MIROC-ESM_rcp60")
@@ -84,19 +79,19 @@ nc_to_csv(Datfile = "landCoverFrac_Lmon_MIROC-ESM-CHEM_rcp60_r1i1p1_200601-21001
 
 # RCP 85
 nc_to_csv_ACCESS(Datfile = "landCoverFrac_Lmon_ACCESS1-0_rcp85_r1i1p1_200601-200601", 
-          sourceDir = "Data/Raw", destDir = "Data/Processed/ACCESS1-0_rcp85")
+                 sourceDir = "Data/Raw", destDir = "Data/Processed/ACCESS1-0_rcp85")
 
 nc_to_csv_ACCESS(Datfile = "landCoverFrac_Lmon_ACCESS1-3_rcp85_r1i1p1_200601-200601", 
-          sourceDir = "Data/Raw", destDir = "Data/Processed/ACCESS1-3_rcp85")
+                 sourceDir = "Data/Raw", destDir = "Data/Processed/ACCESS1-3_rcp85")
 
-nc_to_csv(Datfile = "landCoverFrac_Lmon_IPSL-CM5A-LR_rcp85_r1i1p1_200601-230012", 
-          sourceDir = "Data/Raw", destDir = "Data/Processed/IPSL-CM5A-LR_rcp85")
+nc_to_csv_IPSL(Datfile = "landCoverFrac_Lmon_IPSL-CM5A-LR_rcp85_r1i1p1_200601-230012", 
+               sourceDir = "Data/Raw", destDir = "Data/Processed/IPSL-CM5A-LR_rcp85")
 
-nc_to_csv(Datfile = "landCoverFrac_Lmon_IPSL-CM5A-MR_rcp85_r1i1p1_200601-210012", 
-          sourceDir = "Data/Raw", destDir = "Data/Processed/IPSL-CM5A-MR_rcp85")
+nc_to_csv_IPSL(Datfile = "landCoverFrac_Lmon_IPSL-CM5A-MR_rcp85_r1i1p1_200601-210012", 
+               sourceDir = "Data/Raw", destDir = "Data/Processed/IPSL-CM5A-MR_rcp85")
 
-nc_to_csv(Datfile = "landCoverFrac_Lmon_IPSL-CM5B-LR_rcp85_r1i1p1_200601-210012", 
-          sourceDir = "Data/Raw", destDir = "Data/Processed/IPSL-CM5B-LR_rcp85")
+nc_to_csv_IPSL(Datfile = "landCoverFrac_Lmon_IPSL-CM5B-LR_rcp85_r1i1p1_200601-210012", 
+               sourceDir = "Data/Raw", destDir = "Data/Processed/IPSL-CM5B-LR_rcp85")
 
 nc_to_csv(Datfile = "landCoverFrac_Lmon_MIROC-ESM_rcp85_r1i1p1_200601-210012", 
           sourceDir = "Data/Raw", destDir = "Data/Processed/MIROC-ESM_rcp85")
@@ -116,15 +111,105 @@ nc_to_csv(Datfile = "landCoverFrac_Lmon_MRI-ESM1_rcp85_r1i1p1_200601-210012",
 
 
 ##### ------------------------ Plotting ---------------------------- #####
-# plot discret years
-# RCP 26
+### plot discret years to test
 Aus_Vegetation_Plot_Discret(sourceDir = "Data/Processed/MIROC-ESM_rcp26", 
                             destDir = "Plots/Discret/MIROC-ESM_rcp26")
 
+### plot continuous year plots within the same pdf file
+## RCP 26
+Aus_Vegetation_Plot_Continuous(sourceDir = "Data/Processed/MIROC-ESM_rcp26",
+                               destDir = "Plots/Continuous/MIROC-ESM_rcp26")
+
+Aus_Vegetation_Plot_Continuous(sourceDir = "Data/Processed/IPSL-CM5A-LR_rcp26",
+                               destDir = "Plots/Continuous/IPSL-CM5A-LR_rcp26")
+
+Aus_Vegetation_Plot_Continuous(sourceDir = "Data/Processed/IPSL-CM5A-MR_rcp26",
+                               destDir = "Plots/Continuous/IPSL-CM5A-MR_rcp26")
+
+Aus_Vegetation_Plot_Continuous(sourceDir = "Data/Processed/MIROC-ESM-CHEM_rcp26",
+                               destDir = "Plots/Continuous/MIROC-ESM-CHEM_rcp26")
+
+Aus_Vegetation_Plot_Continuous(sourceDir = "Data/Processed/MPI-ESM-LR_rcp26",
+                               destDir = "Plots/Continuous/MPI-ESM-LR_rcp26")
+
+Aus_Vegetation_Plot_Continuous(sourceDir = "Data/Processed/MPI-ESM-MR_rcp26",
+                               destDir = "Plots/Continuous/MPI-ESM-MR_rcp26")
+
+## RCP 45
+Aus_Vegetation_Plot_Continuous(sourceDir = "Data/Processed/ACCESS1-0_rcp45",
+                               destDir = "Plots/Continuous/ACCESS1-0_rcp45")
+
+Aus_Vegetation_Plot_Continuous(sourceDir = "Data/Processed/ACCESS1-3_rcp45",
+                               destDir = "Plots/Continuous/ACCESS1-3_rcp45")
+
+Aus_Vegetation_Plot_Continuous(sourceDir = "Data/Processed/IPSL-CM5A-LR_rcp45",
+                               destDir = "Plots/Continuous/IPSL-CM5A-LR_rcp45")
+
+Aus_Vegetation_Plot_Continuous(sourceDir = "Data/Processed/IPSL-CM5A-MR_rcp45",
+                               destDir = "Plots/Continuous/IPSL-CM5A-MR_rcp45")
+
+Aus_Vegetation_Plot_Continuous(sourceDir = "Data/Processed/IPSL-CM5B-LR_rcp45",
+                               destDir = "Plots/Continuous/IPSL-CM5B-LR_rcp45")
+
+Aus_Vegetation_Plot_Continuous(sourceDir = "Data/Processed/MIROC-ESM_rcp45",
+                               destDir = "Plots/Continuous/MIROC-ESM_rcp45")
+
+Aus_Vegetation_Plot_Continuous(sourceDir = "Data/Processed/MIROC-ESM-CHEM_rcp45",
+                               destDir = "Plots/Continuous/MIROC-ESM-CHEM_rcp45")
+
+Aus_Vegetation_Plot_Continuous(sourceDir = "Data/Processed/MPI-ESM-LR_rcp45",
+                               destDir = "Plots/Continuous/MPI-ESM-LR_rcp45")
+
+Aus_Vegetation_Plot_Continuous(sourceDir = "Data/Processed/MPI-ESM-MR_rcp45",
+                               destDir = "Plots/Continuous/MPI-ESM-MR_rcp45")
 
 
-sourceDir <- "~/Documents/Research/Projects/Oz_vegetation_model/Github/Data/Processed/MIROC-ESM_rcp26"
-destDir <- "~/Documents/Research/Projects/Oz_vegetation_model/Github/Plots/Discret/MIROC-ESM_rcp26"
+## RCP 60
+Aus_Vegetation_Plot_Continuous(sourceDir = "Data/Processed/IPSL-CM5A-LR_rcp60",
+                               destDir = "Plots/Continuous/IPSL-CM5A-LR_rcp60")
+
+Aus_Vegetation_Plot_Continuous(sourceDir = "Data/Processed/IPSL-CM5A-MR_rcp60",
+                               destDir = "Plots/Continuous/IPSL-CM5A-MR_rcp60")
+
+Aus_Vegetation_Plot_Continuous(sourceDir = "Data/Processed/MIROC-ESM_rcp60",
+                               destDir = "Plots/Continuous/MIROC-ESM_rcp60")
+
+Aus_Vegetation_Plot_Continuous(sourceDir = "Data/Processed/MIROC-ESM-CHEM_rcp60",
+                               destDir = "Plots/Continuous/MIROC-ESM-CHEM_rcp60")
+
+
+## RCP 85
+Aus_Vegetation_Plot_Continuous(sourceDir = "Data/Processed/ACCESS1-0_rcp85",
+                               destDir = "Plots/Continuous/ACCESS1-0_rcp85")
+
+Aus_Vegetation_Plot_Continuous(sourceDir = "Data/Processed/ACCESS1-3_rcp85",
+                               destDir = "Plots/Continuous/ACCESS1-3_rcp85")
+
+Aus_Vegetation_Plot_Continuous(sourceDir = "Data/Processed/IPSL-CM5A-LR_rcp85",
+                               destDir = "Plots/Continuous/IPSL-CM5A-LR_rcp85")
+
+Aus_Vegetation_Plot_Continuous(sourceDir = "Data/Processed/IPSL-CM5A-MR_rcp85",
+                               destDir = "Plots/Continuous/IPSL-CM5A-MR_rcp85")
+
+Aus_Vegetation_Plot_Continuous(sourceDir = "Data/Processed/IPSL-CM5B-LR_rcp85",
+                               destDir = "Plots/Continuous/IPSL-CM5B-LR_rcp85")
+
+Aus_Vegetation_Plot_Continuous(sourceDir = "Data/Processed/MIROC-ESM_rcp85",
+                               destDir = "Plots/Continuous/MIROC-ESM_rcp85")
+
+Aus_Vegetation_Plot_Continuous(sourceDir = "Data/Processed/MIROC-ESM-CHEM_rcp85",
+                               destDir = "Plots/Continuous/MIROC-ESM-CHEM_rcp85")
+
+Aus_Vegetation_Plot_Continuous(sourceDir = "Data/Processed/MPI-ESM-LR_rcp85",
+                               destDir = "Plots/Continuous/MPI-ESM-LR_rcp85")
+
+Aus_Vegetation_Plot_Continuous(sourceDir = "Data/Processed/MPI-ESM-MR_rcp85",
+                               destDir = "Plots/Continuous/MPI-ESM-MR_rcp85")
+
+Aus_Vegetation_Plot_Continuous(sourceDir = "Data/Processed/MRI-ESM1_rcp85",
+                               destDir = "Plots/Continuous/MRI-ESM1_rcp85")
+
+
 
 ##### ------------------------ Clear workspace ---------------------------- #####
 rm(list=ls(all=TRUE))
--- modules/Australia_CMIP5_plot.R
+++ modules/Australia_CMIP5_plot.R
@@ -77,14 +77,11 @@ Aus_Vegetation_Plot_Continuous <- function(sourceDir, destDir) {
         
         dd <- read.csv(inName)
         
-        # plot year 2006-01, 2036-01, 2066-01, 2096-01 for each pft
-        p1 <- subset(dd, year == 2006)
-        p2 <- subset(dd, year == 2036)
-        p3 <- subset(dd, year == 2066)
-        p4 <- subset(dd, year == 2096)
-        
         oName <- gsub(".csv", ".pdf", DatFiles[i])
         outName <- paste0(destDir, "/", oName)
+        
+        # plot all year and month for each pft
+        year.list <- unique(dd$year)
         
         # color range
         rg <- seq(0, 100, by=10)
@@ -98,32 +95,27 @@ Aus_Vegetation_Plot_Continuous <- function(sourceDir, destDir) {
             mgp = c(3, 1, 0))  
         set.panel(2,2)
         
-        # p1
-        with(p1, quilt.plot(lon, lat, jan, xlim=c(110, 160), ylim=c(-50, -9),
-                            add.legend=T, breaks=rg, col=rev(topo.colors(10)),
-                            nx = 10, ny = 10, main = "Year 2006 01"))
-        world(add=T, col=adjustcolor("grey", 0.8))
-        
-        
-        # p2
-        with(p2, quilt.plot(lon, lat, jan, xlim=c(110, 160), ylim=c(-50, -9),
-                            add.legend=T, breaks=rg, col=rev(topo.colors(10)),
-                            nx = 10, ny = 10, main = "Year 2036 01"))
-        world(add=T, col=adjustcolor("grey", 0.8))
-        
-        # p3
-        with(p3, quilt.plot(lon, lat, jan, xlim=c(110, 160), ylim=c(-50, -9),
-                            add.legend=T, breaks=rg, col=rev(topo.colors(10)),
-                            nx = 10, ny = 10, main = "Year 2066 01"))
-        world(add=T, col=adjustcolor("grey", 0.8))
-        
-        # p4
-        with(p4, quilt.plot(lon, lat, jan, xlim=c(110, 160), ylim=c(-50, -9),
-                            add.legend=T, breaks=rg, col=rev(topo.colors(10)),
-                            nx = 10, ny = 10, main = "Year 2096 01"))
-        world(add=T, col=adjustcolor("grey", 0.8))
-        
+        for (j in year.list) {
+            # subsetting df for each year
+            p1 <- subset(dd, year == j)
+            
+            # obtain month column information
+            month.col <- c(5:16)
+            
+            for (k in month.col) {
+                
+                # title
+                tl <- paste0("Year ", j, "Month ", names[p1]$k)
+                
+                # plotting
+                quilt.plot(p1$lon, p1$lat, p1[,k], xlim=c(110, 160), ylim=c(-50, -9),
+                           add.legend=T, breaks=rg, col=rev(topo.colors(10)),
+                           nx = 10, ny = 10, main = tl)
+                
+                # add world map
+                world(add=T, col=adjustcolor("grey", 0.8))
+            }   # k
+        }   # j
         dev.off()
-    }
-    
+    }   # i
 }
